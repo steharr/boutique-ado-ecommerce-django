29c4937e39e1b4373b0b96384a784178e9e549d4
Stephen Harrold <steharrold@gmail.com>
2021-12-31 21:06:33 +0000

    connect and test handler with stripe

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,6 +7,22 @@ class StripeWH_Handler():
         self.request = request
 
     def handle_event(self, event):
-        """Handle a generic/unknown/unexpected webhook event"""
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f"Unhandled webhook recieved: {event['type']}", status=200)
+
+    def handle_payment_intent_succeded(self, event):
+        """
+        Handle a payment_intent.succeded webhook from Stripe
+        """
+        return HttpResponse(content=f"Webhook recieved: {event['type']}",
+                            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle a payment_intent.payment_failed webhook from Stripe
+        """
         return HttpResponse(content=f"Webhook recieved: {event['type']}",
                             status=200)
