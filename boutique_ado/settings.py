ac2f966d3614ae5b4773ca69204b189c339dd3fa
Stephen Harrold <steharrold@gmail.com>
2022-01-15 13:53:50 +0000

    setup start of deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -15,6 +15,8 @@ import os
 if os.path.exists('env.py'):
     import env
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
